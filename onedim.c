b72eaa9b8c6989cd6f0ae5c7947c22243603fef5
PRAGATHI RAJ <pragathijacky@gmail.com>
2026-07-21 12:32:40 +0530

    Add files via upload

---
+++ onedim.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+void main(){
+    int n , i;
+    int arr[999];
+    printf("Enter the size of array: ");
+    scanf("%d",&n);
+    printf("Enter the elements of array: ");
+    for(i=0;i<n;i++){
+        {
+            printf("Enter  arr[%dg]: ",i);
+            scanf("%d",&arr[i]);
+        }
+}
+void read()
+{
+    /*this function  is from printing helloworkd */
+    printf("hello world");
+
+}
+printf("array element  are ");
+for(i=0;i<n;i++){
+    printf("%d ",arr[i]);
+}
+    }
