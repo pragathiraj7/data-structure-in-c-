1d62fe2bbb6c5e13d1e1bdf4e43ec97931d1b5d9
PRAGATHI RAJ <pragathijacky@gmail.com>
2026-07-28 23:34:15 +0530

    Add files via upload

---
+++ free.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int *ptr;
+
+    ptr = (int *)malloc(sizeof(int));
+
+    *ptr = 100;
+    printf("Value = %d\n", *ptr);
+
+    free(ptr);
+    ptr = NULL;
+
+    return 0;
+}
